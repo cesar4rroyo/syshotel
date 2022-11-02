74b0ac1ec7eb8471d527eb6a7efe1b9edbd21145
Cesar Arroyo <cesardavid_09@hotmail.com>
2022-11-01 19:19:00 -0500

    feat: adding new migration tables

--- app/Models/People.php
+++ app/Models/People.php
@@ -4,8 +4,49 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class People extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'people';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'dni',
+        'email',
+        'phone',
+        'age',
+        'birthday',
+        'social_reason',
+        'ruc',
+        'address',
+        'district_id',
+        'province_id',
+        'department_id',
+        'country_id',
+        'notes',
+    ];
+
+    public function district()
+    {
+        return $this->belongsTo(District::class);
+    }
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
