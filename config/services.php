03ef09ea834e53d6711d29d2042892df032cea89
Cesar Arroyo <cesardavid_09@hotmail.com>
2022-11-03 21:46:39 -0500

    feat: adding services to consume sunat/reniec

--- config/services.php
+++ config/services.php
@@ -31,4 +31,16 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'reniec' => [
+        'token' => env('RENIEC_TOKEN'),
+        'fe' => 'N',
+        'url' => env('BASE_URL_CLIENT_DNI'),
+    ],
+
+    'sunat' => [
+        'token' => env('SUNAT_TOKEN'),
+        'fe' => 'N',
+        'url' => env('BASE_URL_CLIENT_RUC'),
+    ],
+
 ];
